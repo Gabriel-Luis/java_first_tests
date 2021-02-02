2f556315b8e251fcb2b4029c2dc84c991b0d579e
Gabriel Luis de Oliveira <51294919+Gabriel-Luis@users.noreply.github.com>
2021-02-02 19:58:59 -0300

    Add files via upload

---
+++ bytebank/src/bytebank/CriaConta.java
@@ -0,0 +1,18 @@
+package bytebank;
+
+public class CriaConta {
+	public static void main(String[] args) {
+		
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		System.out.println(primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		
+		System.out.println(segundaConta.saldo);
+		System.out.println("ashshshsh"
+				+ "z"+segundaConta.agencia);
+	}
+}
+	
